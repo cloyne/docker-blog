8f4d803acd1b3aaedb0dc72aceb1a69405b9ceb7
Cloyne Network Steward <clonm@bsc.coop>
2019-07-28 21:27:04 -0700

    add access.log

--- multitail.sh
+++ multitail.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-multitail -cS apache_error development/log/nginx/error.log \
-    -cS mysql development/log/mysql/error.log
-    # -cS apache -I development/log/nginx/access.log
+multitail -s 2 -sn 2,1 -cS apache_error development/log/nginx/error.log \
+    -cS mysql development/log/mysql/error.log \
+    -cS apache development/log/nginx/access.log
