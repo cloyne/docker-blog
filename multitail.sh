12b5aae3de4d6c30a36850f304ecd78b181a6ecd
Cloyne Network Steward <clonm@bsc.coop>
2019-08-20 16:24:31 -0700

    improve multitail script

--- multitail.sh
+++ multitail.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-multitail -s 2 -sn 2,1 -cS apache_error development/log/nginx/error.log \
-    -cS mysql development/log/mysql/error.log \
-    -cS apache development/log/nginx/access.log
+sudo multitail -s 2 -sn 1,2 -cS mysql development/log/mysql/error.log \
+    -cS apache development/log/nginx/access.log \
+    -cS apache_error development/log/nginx/error.log \
